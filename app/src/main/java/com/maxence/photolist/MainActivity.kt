902fa293061c425a86f87e6bde23ed6403f03453
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-04-19 18:16:29 +0200

    Cleaning code

--- app/src/main/java/com/maxence/photolist/MainActivity.kt
+++ app/src/main/java/com/maxence/photolist/MainActivity.kt
@@ -2,43 +2,38 @@ package com.maxence.photolist
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import coil.compose.rememberImagePainter
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.maxence.photolist.ui.theme.PhotoListTheme
-import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var cameraExecutor: ExecutorService
-
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-            Log.i("kilo", "Permission granted")
+            Log.i("PhotoList", "Permission granted")
         } else {
-            Log.i("kilo", "Permission denied")
+            Log.i("PhotoList", "Permission denied")
         }
     }
+
+    private lateinit var viewModel: PhotoViewModel
 
     private fun requestCameraPermission() {
         when {
@@ -46,13 +41,13 @@ class MainActivity : ComponentActivity() {
                 this,
                 Manifest.permission.CAMERA
             ) == PackageManager.PERMISSION_GRANTED -> {
-                Log.i("kilo", "Permission previously granted")
+                Log.i("PhotoList", "Permission previously granted")
             }
 
             ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
                 Manifest.permission.CAMERA
-            ) -> Log.i("kilo", "Show camera permissions dialog")
+            ) -> Log.i("PhotoList", "Show camera permissions dialog")
 
             else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
@@ -62,51 +57,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PhotoListTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    NavComponent()
-
-//                    if (shouldShowCamera.value) {
-//                        CameraView(
-//                            outputDirectory = outputDirectory,
-//                            executor = cameraExecutor,
-//                            onImageCaptured = ::handleImageCapture,
-//                            onError = { Log.e("kilo", "View error:", it) }
-//                        )
-//                    }
-//                    if (shouldShowPhoto.value) {
-//                        Image(
-//                            painter = rememberImagePainter(photoUri),
-//                            contentDescription = null,
-//                            modifier = Modifier.fillMaxSize()
-//                        )
-//                    }
+                    viewModel = viewModel(
+                        factory = PhotoViewModelFactory(this.application)
+                    )
+                    NavComponent(
+                        viewModel
+                    )
                 }
             }
         }
         requestCameraPermission()
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onDestroy() {
+        viewModel.outputDirectory.listFiles()?.forEach {
+            it.delete()
+        }
         super.onDestroy()
-        cameraExecutor.shutdown()
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PhotoListTheme {
-        Greeting("Android")
     }
 }
