d1ffa7a3ae92121185fcb6772a1af69f10c60db9
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-04-19 18:15:56 +0200

    Delete unused var and add fun

--- app/src/main/java/com/maxence/photolist/PhotoViewModel.kt
+++ app/src/main/java/com/maxence/photolist/PhotoViewModel.kt
@@ -3,40 +3,41 @@ package com.maxence.photolist
 import android.app.Application
 import android.net.Uri
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import java.io.File
-import java.util.concurrent.ExecutorService
 
-class PhotoViewModel(val application: Application): ViewModel() {
+class PhotoViewModel(private val application: Application) : ViewModel() {
 
     var outputDirectory: File = setOutputDirectory()
     private lateinit var photoUri: Uri
 
-    private var shouldShowPhoto: MutableState<Boolean> = mutableStateOf(false)
-    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
     val listPhoto: MutableList<PhotoItem> = mutableListOf()
 
-
-    fun addPhoto(photo: PhotoItem) {
-        listPhoto.add(photo)
-    }
-
-    private fun getOutputDirectory() {
+    private fun setOutputDirectory(): File {
         val mediaDir = application.externalMediaDirs.firstOrNull()?.let {
             File(it, application.getString(R.string.app_name)).apply { mkdirs() }
         }
 
-        outputDirectory = if (mediaDir != null && mediaDir.exists()) mediaDir else application.filesDir
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else application.filesDir
     }
 
-    private fun handleImageCapture(uri: Uri) {
-        Log.i("kilo", "Image captured: $uri")
-        shouldShowCamera.value = false
+    fun fetchPhotos() {
+        listPhoto.clear()
+        val files = outputDirectory.listFiles()
+        if (files != null) {
+            for (file in files) {
+                Log.d("PhotoViewModel", "file: $file")
+                listPhoto.add(PhotoItem(id = file.name, title = file.absolutePath, uri = file.toUri()))
+            }
+        }
+    }
+
+    fun handleImageCapture(uri: Uri) {
+        Log.i("PhotoList", "Image captured: $uri")
         photoUri = uri
-        shouldShowPhoto.value = true
     }
 }
