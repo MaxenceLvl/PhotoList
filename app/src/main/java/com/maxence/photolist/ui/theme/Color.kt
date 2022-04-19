9ce185ab7058b40c9fc6e1023e253e909d89c61e
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-04-19 18:14:21 +0200

    Update Color
    PrimaryColor
    SecondaryColor
    Status bar

--- app/src/main/java/com/maxence/photolist/ui/theme/Color.kt
+++ app/src/main/java/com/maxence/photolist/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val blueMarine = Color(0xFF164255)
+val redPink = Color(0xFFE05657)
