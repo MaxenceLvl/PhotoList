f71db7d28eda8da5b68aa035f9fe132e5b2b1c1f
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-04-19 18:17:11 +0200

    Pimp UI

--- app/src/main/java/com/maxence/photolist/HomeView.kt
+++ app/src/main/java/com/maxence/photolist/HomeView.kt
@@ -1,33 +1,41 @@
 package com.maxence.photolist
 
-import android.net.Uri
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import coil.compose.rememberImagePainter
 
 @Composable
-fun HomeView(navController: NavHostController, vm: PhotoViewModel) {
+fun HomeScreen(navController: NavHostController, viewModel: PhotoViewModel) {
+    HomeView(navController = navController, photos = viewModel.listPhoto)
+}
+
+@Composable
+fun HomeView(navController: NavHostController, photos: List<PhotoItem>) {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(12.dp),
     ) {
         Column(
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Column(
-                modifier = Modifier.verticalScroll(rememberScrollState()),
-                verticalArrangement = Arrangement.spacedBy(2.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                vm.listPhoto.forEach {
-                    PhotoRaw(photo = it)
+                photos.forEach {
+                    PhotoRaw(photo = it) {
+                        navController.navigate("detail/${it.id}")
+                    }
                 }
             }
         }
